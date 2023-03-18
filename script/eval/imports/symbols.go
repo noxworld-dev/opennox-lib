aed4e42765bfc6305239df6f394efea5c61fbf65
Denys Smirnov <dennwc@pm.me>
2023-03-18 15:38:21 +0200

    eval: Whitelist image/color package.

--- script/eval/imports/symbols.go
+++ script/eval/imports/symbols.go
@@ -4,7 +4,7 @@ import "reflect"
 
 //go:generate yaegi extract errors math math/rand time sort
 //go:generate yaegi extract bytes strings strconv unicode io fmt
-//go:generate yaegi extract context
+//go:generate yaegi extract context image/color
 //go:generate yaegi extract github.com/noxworld-dev/opennox-lib/types
 //go:generate yaegi extract github.com/noxworld-dev/opennox-lib/object
 //go:generate yaegi extract github.com/noxworld-dev/opennox-lib/player
