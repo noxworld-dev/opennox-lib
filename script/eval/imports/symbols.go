3fd4b5434dc8c6b8eff76ae2f485c415aaf35d11
Denys Smirnov <dennwc@pm.me>
2023-04-16 19:37:32 +0300

    script/eval: Make sure we expose stdlib.

--- script/eval/imports/symbols.go
+++ script/eval/imports/symbols.go
@@ -12,3 +12,15 @@ import "reflect"
 //go:generate goimports -w .
 
 var Symbols = make(map[string]map[string]reflect.Value)
+
+// This block ensures that we compile the stdlib bindings.
+// It may fail if the compiler version doesn't match the one used to generate bindings.
+// To fix this, we remove build tags from stdlib files and remove any bindings introduced in newer Go versions.
+
+var (
+	_ _fmt_Formatter
+	_ _io_Closer
+	_ _math_rand_Source
+	_ _sort_Interface
+	_ _image_color_Color
+)
